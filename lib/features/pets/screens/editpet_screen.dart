bbb0a7e600c4ad8e0ee6d3e4cc87060ea308bc69
Natawat Hebnak <minin0la17@gmail.com>
2023-11-14 00:44:02 +0800

    added loading

--- lib/features/pets/screens/editpet_screen.dart
+++ lib/features/pets/screens/editpet_screen.dart
@@ -58,6 +58,7 @@ class _EditPetScreenState extends ConsumerState<EditPetScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final isLoading = ref.watch(petControllerProvider);
     return ref.watch(getPetByIDProvider(widget.petID)).when(
         data: (pet) => Scaffold(
               // backgroundColor: Palla.,
@@ -68,51 +69,56 @@ class _EditPetScreenState extends ConsumerState<EditPetScreen> {
                   TextButton(onPressed: () {}, child: const Text("Save"))
                 ],
               ),
-              body: Column(children: [
-                GestureDetector(
-                  onTap: selectPetImage,
-                  child: DottedBorder(
-                      child: Container(
-                    width: double.infinity,
-                    height: 150,
-                    decoration:
-                        BoxDecoration(borderRadius: BorderRadius.circular(10)),
-                    child: avatarFile != null
-                        ? Image.file(avatarFile!)
-                        : pet.avatar.isEmpty ||
-                                pet.avatar == Constants.avatarDefault
-                            ? const Center(
-                                child: Icon(
-                                Icons.camera_alt_outlined,
-                                size: 40,
-                              ))
-                            : Image.network(
-                                pet.avatar,
-                                fit: BoxFit.cover,
-                              ),
-                  )),
-                ),
-                const SizedBox(height: 20),
-                TextField(
-                  controller: _petNameController,
-                  decoration: const InputDecoration(
-                      labelText: "Pet Name", border: OutlineInputBorder()),
-                ),
-                OutlinedButton(
-                    onPressed: () => save(pet),
-                    style: ElevatedButton.styleFrom(
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20)),
-                        padding: const EdgeInsets.symmetric(horizontal: 25)),
-                    child: const Text("Save")),
-                OutlinedButton(
-                    onPressed: () => deletePet(ref, context, pet),
-                    style: ElevatedButton.styleFrom(
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20)),
-                        padding: const EdgeInsets.symmetric(horizontal: 25)),
-                    child: const Text("Delete")),
-              ]),
+              body: isLoading
+                  ? const Loader()
+                  : Column(children: [
+                      GestureDetector(
+                        onTap: selectPetImage,
+                        child: DottedBorder(
+                            child: Container(
+                          width: double.infinity,
+                          height: 150,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10)),
+                          child: avatarFile != null
+                              ? Image.file(avatarFile!)
+                              : pet.avatar.isEmpty ||
+                                      pet.avatar == Constants.avatarDefault
+                                  ? const Center(
+                                      child: Icon(
+                                      Icons.camera_alt_outlined,
+                                      size: 40,
+                                    ))
+                                  : Image.network(
+                                      pet.avatar,
+                                      fit: BoxFit.cover,
+                                    ),
+                        )),
+                      ),
+                      const SizedBox(height: 20),
+                      TextField(
+                        controller: _petNameController,
+                        decoration: const InputDecoration(
+                            labelText: "Pet Name",
+                            border: OutlineInputBorder()),
+                      ),
+                      OutlinedButton(
+                          onPressed: () => save(pet),
+                          style: ElevatedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(20)),
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 25)),
+                          child: const Text("Save")),
+                      OutlinedButton(
+                          onPressed: () => deletePet(ref, context, pet),
+                          style: ElevatedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(20)),
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 25)),
+                          child: const Text("Delete")),
+                    ]),
             ),
         loading: () => const Loader(),
         error: (error, stack) => ErrorText(error: error.toString()));
